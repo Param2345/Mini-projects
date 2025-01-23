0cfa4adfc832b3d945459383f99f275beaef71d1
Param2345 <106765887+Param2345@users.noreply.github.com>
2025-01-23 21:06:31 +0530

    Popup Form

---
+++ Popup form/README.md
@@ -0,0 +1,8 @@
+/\_----------------------------
+Popup form
+----------------------------\*/
+
+. In this project you will learn about popup form
+. Html consists the buttons and the form
+. Css consits its styling
+.Javascript consists its functions
---
+++ Popup form/js/main.js
@@ -0,0 +1,11 @@
+// open form
+
+function openForm() {
+    const form = document.getElementById("form")
+    form.style.display = 'block';
+}
+
+function closeForm() {
+    const form = document.getElementById("form")
+    form.style.display = 'none';
+}
