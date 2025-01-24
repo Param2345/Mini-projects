dc9f3158a9b9ecc843115d080236bfa715478b47
Param2345 <106765887+Param2345@users.noreply.github.com>
2025-01-24 14:36:15 +0530

    Add files via upload

---
+++ Digital Clock/Js/main.js
@@ -0,0 +1,14 @@
+// const
+const hrs = document.getElementById("hrs")
+const min = document.getElementById("min")
+const sec = document.getElementById("sec")
+
+//set interval
+
+setInterval(() => {
+    let currentTime = new Date();
+
+    hrs.innerHTML = (currentTime.getHours() < 10?"0":"") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes() < 10?"0":"") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds() < 10?"0":"") + currentTime.getSeconds();
+},1000)
